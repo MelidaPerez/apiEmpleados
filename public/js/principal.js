00ca64a343e8771cd959feb01d26ecf75f4c449e
MelidaPerez <cristyordo@gmail.com>
2020-10-21 22:59:57 -0600

    empleados vue

---
+++ public/js/principal.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el : '#pp',
+
+    data : {
+
+        lista : [
+
+            {ID: '01', Codigo: '02Mkj', Nombres: 'Luis Pedro', Apellidos: 'Rosales Hernandez', Direccion: 'Antigua', Telefono: '12345687', Fecha_de_nacimiento: '1999-08-10', Puesto: 'diseñador'}
+        ]
+    }
+
+})
